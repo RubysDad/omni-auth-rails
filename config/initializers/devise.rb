4ef826af6ee83577c177a7ecd8539ff06e6358bd
Ruby's Dad <markandtabithamorales@gmail.com>
2018-03-02 14:09:03 +0000

    updated devise config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -277,7 +277,7 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   
-  config.omniauth :facebook, "APP_ID", "APP_SECRET", callback_url: "https://omnitestfacebook.herokuapp.com/users/auth/facebook/callback"
+  config.omniauth :facebook, callback_url: "https://omnitestfacebook.herokuapp.com/users/auth/facebook/callback"
     # Other configuration...
   
     # ==> OmniAuth
